9b2f01e378312e31067ffc1fde75ba2b50ce935f
ushironoko <apple19940820@gmail.com>
2020-12-28 17:57:52 +0900

    fix style

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-between items-center font-mono w-full mb-6 h-12">
+  <header class="flex justify-between items-center font-mono w-full my-6">
     <nuxt-link class="no-underline hover:no-underline" to="/">
       <p class="text-black text-2xl font-extrabold">ushironoko.me</p>
     </nuxt-link>
