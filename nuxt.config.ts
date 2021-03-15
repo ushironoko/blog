3943bae8253008df8993085045ba0060a710d7a8
ushironoko <apple19940820@gmail.com>
2021-03-15 18:22:54 +0900

    fix: add htmlAttrs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,10 +38,12 @@ const config: NuxtConfig = {
   srcDir: 'src',
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'ushironoko.me',
+    htmlAttrs: {
+      lang: 'ja',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
