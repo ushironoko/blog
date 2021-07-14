350e4b9c7e5ed54a179a3874daea5f52a4e90eed
ushironoko <apple19940820@gmail.com>
2021-07-14 23:20:43 +0900

    スキップリンクを追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="dark:bg-black dark:text-white">
+    <a class="sr-only" href="#main">メインへジャンプ</a>
     <div class="max-w-2xl px-4 py-4 mx-auto text-base leading-loose">
       <TheHeader />
-      <main>
+      <main id="main">
         <Nuxt />
       </main>
     </div>
