e0b463d0921096fc1dd09cb945cae63daf64630e
ushironoko <apple19940820@gmail.com>
2021-10-12 01:19:52 +0900

    add lost judgment

---
+++ src/components/YoutubeEmbedVideo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="video-container">
+    <div class="video-player">
+      <YoutubeEmbedVideoChild :src="url" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import YoutubeEmbedVideoChild from './YoutubeEmbedVideoChild.vue'
+
+export default defineComponent({
+  components: {
+    YoutubeEmbedVideoChild,
+  },
+  props: {
+    youtubeId: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const url = `https://www.youtube.com/embed/${props.youtubeId}`
+
+    return {
+      url,
+    }
+  },
+})
+</script>
---
+++ src/components/YoutubeEmbedVideoChild.vue
@@ -0,0 +1,17 @@
+<template>
+  <iframe class="w-full h-full" frameborder="0" allowfullscreen></iframe>
+</template>
+
+<style lang="postcss" scoped>
+iframe {
+  width: 100%;
+  height: 100%;
+  min-height: 211px;
+}
+
+@media (min-width: 768px) {
+  iframe {
+    min-height: 378px;
+  }
+}
+</style>
