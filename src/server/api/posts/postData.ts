b927ff03c93f2b1ba462515109c868ba32a7ae51
ushironoko <apple19940820@gmail.com>
2023-04-19 20:42:29 +0900

    サニタイズ外す

--- src/server/api/posts/postData.ts
+++ src/server/api/posts/postData.ts
@@ -5,7 +5,6 @@ import { remark } from 'remark';
 import remarkExternalLinks from 'remark-external-links';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
-import rehypeSanitize from 'rehype-sanitize';
 import rehypeHighlight from 'rehype-highlight';
 import rehypeStringify from 'rehype-stringify';
 
@@ -25,7 +24,6 @@ export default defineEventHandler(async (event) => {
     })
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeSanitize)
     .use(rehypeHighlight)
     .use(rehypeStringify)
     .process(matterResult.content);
