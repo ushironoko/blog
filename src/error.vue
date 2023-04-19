6f23908b3f655cb6db7cd3659d399336dcad8dc9
ushironoko <apple19940820@gmail.com>
2023-04-19 16:13:26 +0900

    redirect old path

---
+++ src/error.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute();
+
+if (route.path.startsWith('/articles')) {
+  console.log('route.path: ', route.path);
+  const redirectPath = route.path
+    .replace('/articles/', '/posts/')
+    .replace('/2022', '')
+    .replace('/2021', '')
+    .replace('/2020', '');
+
+  navigateTo(redirectPath);
+} else {
+  navigateTo('/');
+}
+</script>
